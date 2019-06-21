7c869227c8f28a43cc2bd92e151540fb6cd986ca
Jeppe Ekstrand Halkjaer Madsen <jehm@sund.ku.dk>
2019-06-21 11:54:37 +0200

    treatment before minimum of cens and event

--- R/weirdWay.R
+++ R/weirdWay.R
@@ -2,7 +2,8 @@
 rm(list=ls())
 set.seed(07062019)
 library(survival)
-source("~/Dropbox/phd/CTC/R/estimator.R")
+source("~/Dropbox/phd/Package/CaseTimeControl/R/hazardEst.R")
+source("~/Dropbox/phd/Package/CaseTimeControl/R/ctc.R")
 
 ## Parameters
 n <- 1e5
@@ -11,7 +12,7 @@ lambda <- rep(.05/13,n)
 tau <- 13
 D1 <- 6
 D2 <- 3/4
-gamma <- log(3)
+gamma <- log(1)
 theta <- log(2)
 ## lambda <- lambda*exp(c(rep(0,n/2),rep(theta,n/2)))
 nSim <- 1000
@@ -20,8 +21,8 @@ nSim <- 1000
 estLogit <- or1 <- gammaEst <- numeric(nSim)
 for(i in 1:nSim){
     print(i)
-    ## L <- runif(n,0,25)
-    L <- rexp(n,1/13)
+    L <- runif(n,0,25)
+    ##L <- rexp(n,1/13)
     ## L <- c(runif(n/2,0,25),runif(n/2,0,15))
     u <- runif(n)
     br1 <- 1-exp(-lambda*L)
@@ -32,17 +33,19 @@ for(i in 1:nSim){
     X[br2<=u] <- (-log(1-u[br2<=u])+lambda[br2<=u]*D1*(1-exp(gamma)))/lambda[br2<=u]
     ## X <- rgamma(n, scale=1200,shape=.5)
     ## X <- rlnorm(n,meanlog = log(100))
-    T <- pmin(X,tau)[L<tau]
-    status <- (X<=tau)[L<tau]
-    L <- L[L<tau]
-    tmp <- est(T,status,L,tau,D1,D2,type="long")
+    cond <- (L<pmin(X,tau)&X>3)
+    T <- pmin(X,tau)[cond]
+    status <- (X<=tau)[cond]
+    L <- pmin(L,tau)[cond]
+    tmp <- hazardEst(T,status,L,D1,D2,type="short",leftTruncation=TRUE)
+    ## tmp <- ctc(T,status,L,D1,D2,leftTruncation=TRUE)
     gammaEst[i] <- tmp$Estimate
-    or1[i] <- tmp$Variance
+    or1[i] <- tmp$SE
     print(mean(gammaEst[1:i]))
 }
-mean(or1) + c(0,-1.96,1.96) * sd(or1) / sqrt(nSim)
 exp(mean(gammaEst) + c(0,-1.96,1.96) * sd(gammaEst) / sqrt(nSim))
-mean(gammaEst-qnorm(.975)*sqrt(or1)<log(3)&gammaEst+qnorm(.975)*sqrt(or1)>log(3))
+mean(or1) + c(0,-1.96,1.96) * sd(or1) / sqrt(nSim)
+mean((gammaEst-qnorm(.975)*or1)<log(1)&(gammaEst+qnorm(.975)*or1)>log(1))
 
 
 
