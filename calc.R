46714b20df96af42c1e2c178dbaa048ff96aed0d
Jeppe Ekstrand Halkjaer Madsen <jehm@sund.ku.dk>
2019-09-27 13:20:30 +0200

    more calculations

--- calc.R
+++ calc.R
@@ -4,7 +4,15 @@ D1 <- 6
 D2 <- .75
 lambda <- 1e-3
 
-## Calculations
+## Odds ratio for cases
 (ee <- integrate(function(t) dexp(t,lambda)*(punif(t,0,25)-punif(t-D2,0,25)),0,tau)$value)
 (er <- integrate(function(t) dexp(t,lambda)*(punif(t-D1,0,25)-punif(t-D2-D1,0,25)),0,tau)$value)
-round(ee/er,2)
+(or1 <- ee/er)
+round(or1,2)
+
+## Odds ratio for controls when sampling incorrectly
+(eec <- integrate(function(t) dexp(t,lambda)*punif(t,0,25)/punif(tau,0,25)*(punif(t,0,25)-punif(t-D2,0,25)),0,tau)$value)
+(erc <- integrate(function(t) dexp(t,lambda)*punif(t,0,25)/punif(tau,0,25)*(punif(t-D1,0,25)-punif(t-D2-D1,0,25)),0,tau)$value)
+(or2 <- or1 / (eec / erc))
+round(or2,2)
+
